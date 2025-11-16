7b4311d3adc78281b1e156ea40985524df47dde5
Ayrton Morrison <aymorris@umag.cl>
2025-11-16 18:17:16 -0300

    add: Se refactorizo el codigo anhadiendo la clase ArduinoRobot

--- arduino/src/movement.ino
+++ arduino/src/movement.ino
@@ -2,7 +2,8 @@
 #include <Servo.h>
 
 #define SOUND_SPEED 0.034 // Velocidad del sonido en cm/us
-#define MAX_DISTANCE 20 // Distancia maxima del sensor ultrasonico para detenerse
+#define MAX_DISTANCE 65
+
 
 /**
  * @brief Estructura para definir un motor
@@ -10,110 +11,135 @@
  */
 struct motor
 {
-	int motorIn1; // Input 1 del motor
-	int motorIn2; // Input 2 del motor
-	int motorEnable; // Input Enable del motor
+    int motorIn1; // Input 1 del motor
+    int motorIn2; // Input 2 del motor
+    int motorEnable; // Input Enable del motor
 };
 
 /**
- * @brief Estructura para definir un sensor ultrasonico
+ * @brief Estructura para definir un sensor ultrasónico
  */
 struct ultraSonic
 {
-	int echoPin; // Pin Echo del sensor
-	int trigPin; // Pin Trig del sensor
+    int echoPin; // Pin Echo del sensor
+    int trigPin; // Pin Trig del sensor
 };
 
+
+class ArduinoRobot
+{
+private:
+    motor motor_one;
+    motor motor_two;
+    ultraSonic sonic_sensor;
+
+public:
+    ArduinoRobot(motor m1, motor m2, ultraSonic us);
+    ~ArduinoRobot();
+
+    void init();
+    void advance(int speed_one, int speed_two);
+    void reverse(int speed_one, int speed_two);
+    void stop();
+    float measure_distance();
+};
+
+ArduinoRobot::ArduinoRobot(motor m1, motor m2, ultraSonic us) : motor_one(m1), motor_two(m2), sonic_sensor(us)
+{
+}
+
+ArduinoRobot::~ArduinoRobot()
+{
+}
+
+void ArduinoRobot::init(){
+    // Motores DC
+    pinMode(motor_one.motorIn1, OUTPUT);
+    pinMode(motor_one.motorIn2, OUTPUT);
+    pinMode(motor_one.motorEnable, OUTPUT);
+
+    pinMode(motor_two.motorIn1, OUTPUT);
+    pinMode(motor_two.motorIn2, OUTPUT);
+    pinMode(motor_two.motorEnable, OUTPUT);
+
+    // Sensor ultrasonico
+    pinMode(sonic_sensor.echoPin, INPUT);
+    pinMode(sonic_sensor.trigPin, OUTPUT);
+}
+
+void ArduinoRobot::advance(int speed_one, int speed_two){
+    digitalWrite(motor_one.motorIn1, LOW);
+    digitalWrite(motor_one.motorIn2, HIGH);
+    analogWrite(motor_one.motorEnable, speed_one);
+
+    digitalWrite(motor_two.motorIn1, LOW);
+    digitalWrite(motor_two.motorIn2, HIGH);
+    analogWrite(motor_two.motorEnable, speed_two);
+}
+
+void ArduinoRobot::reverse(int speed_one, int speed_two){
+    digitalWrite(motor_one.motorIn1, HIGH);
+    digitalWrite(motor_one.motorIn2, LOW);
+    analogWrite(motor_one.motorEnable, speed_one);
+
+    digitalWrite(motor_two.motorIn1, HIGH);
+    digitalWrite(motor_two.motorIn2, LOW);
+    analogWrite(motor_two.motorEnable, speed_two);
+}
+
+void ArduinoRobot::stop(){
+    analogWrite(motor_one.motorEnable, 0);
+    analogWrite(motor_two.motorEnable, 0);
+}
+
+float ArduinoRobot::measure_distance(){
+    digitalWrite(sonic_sensor.trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(sonic_sensor.trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(sonic_sensor.trigPin, LOW);
+
+    long duration = pulseIn(sonic_sensor.echoPin, HIGH);
+    float distance = (duration * SOUND_SPEED) / 2;
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    return distance;
+}
+
+
 // Declaracion de pines para componentes
-motor motorOne = {3, 4, 2};
+motor motorOne = {4, 3, 2};
 motor motorTwo = {6, 7, 5};
 
 ultraSonic sonicSensor = {8, 9};
 
+// Creación del objeto robot
+ArduinoRobot robot(motorOne, motorTwo, sonicSensor);
 
-/**
- * @brief Funcion para avanzar el motor
- *
- * @param motorIn1 Input 1 del motor
- * @param motorIn2 Input 2 del motor
- * @param motorEnable Input Enable del motor
- * @param speed Velocidad del motor (0-255)
- */
-void advance_motor(int motorIn1, int motorIn2, int motorEnable, int speed){
-  	digitalWrite(motorIn1, LOW);
-  	digitalWrite(motorIn2, HIGH);
-  	analogWrite(motorEnable, speed);
-}
-
-/**
- * @brief Funcion para retroceder el motor
- *
- * @param motorIn1 Input 1 del motor
- * @param motorIn2 Input 2 del motor
- * @param motorEnable Input Enable del motor
- * @param speed Velocidad del motor (0-255)
- */
-void reverse_motor(int motorIn1, int motorIn2, int motorEnable, int speed){
-  	digitalWrite(motorIn1, HIGH);
-  	digitalWrite(motorIn2, LOW);
-  	analogWrite(motorEnable, speed);
-}
-
-/**
- * @brief Funcion para obtener la distancia medida por el sensor ultrasónico
- *
- * @return float Distancia medida en cm
- */
-float measure_distance(int echoPin, int trigPin){
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
-
-	int duration = pulseIn(echoPin, HIGH);
-	int distance = (duration*SOUND_SPEED)/2;
-
-	Serial.print("Distance: ");
-	Serial.print(distance);
-	Serial.println(" cm");
-
-	return distance;
-}
 
 void setup(){
-	// Motores DC
-  	pinMode(motorOne.motorIn1, OUTPUT);
-  	pinMode(motorOne.motorIn2, OUTPUT);
-  	pinMode(motorOne.motorEnable, OUTPUT);
-
-  	pinMode(motorTwo.motorIn1, OUTPUT);
-  	pinMode(motorTwo.motorIn2, OUTPUT);
-  	pinMode(motorTwo.motorEnable, OUTPUT);
-
-	// Sensor ultrasónico
-	pinMode(sonicSensor.echoPin, INPUT);
-	pinMode(sonicSensor.trigPin, OUTPUT);
-
-	Serial.begin(9600);
+    Serial.begin(9600);
+    robot.init();
 }
 
 void loop(){
 
-	// Medir distancia y avanzar o retroceder según la distancia medida
-	float distance = measure_distance(sonicSensor.echoPin, sonicSensor.trigPin);
+    // Mide distancia y avanza o se detiene segun lo medido
+    float distance = robot.measure_distance();
 
-	if (distance < MAX_DISTANCE){
-		Serial.println("DETENIDO");
-		advance_motor(motorOne.motorIn1, motorOne.motorIn2, motorOne.motorEnable, 0);
-		advance_motor(motorTwo.motorIn1, motorTwo.motorIn2, motorTwo.motorEnable, 0);
-		delay(1000);
-	}
-	else{
-		Serial.println("AVANZANDO");
-		advance_motor(motorOne.motorIn1, motorOne.motorIn2, motorOne.motorEnable, 255);
-		advance_motor(motorTwo.motorIn1, motorTwo.motorIn2, motorTwo.motorEnable, 255);
-		delay(1000);
-	}
+    if (distance < MAX_DISTANCE){
+        Serial.println("Deteniendose");
+        robot.stop();
+        delay(1000);
+    }
+    else{
+        Serial.println("Avanzando");
+        // Se ponen estos valores para compensar velocidades de los motores
+        robot.advance(150, 255);
+        delay(1000);
+    }
 
 }
