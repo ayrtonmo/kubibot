1f46b185b4af3f123ad62339c831b4ecd3e3ea9c
Ayrton Morrison <aymorris@umag.cl>
2025-12-18 15:00:42 -0300

    add: Se hicieron cambios en la interfaz del asistente (audio inicio, audio final)

--- api/client/client.py
+++ api/client/client.py
@@ -1,21 +1,17 @@
 import socketio
-#import requests
 import pvporcupine
 import pvrecorder
 from dotenv import load_dotenv
 import os
-#import wave
 import struct
 import subprocess
 import time
 
 load_dotenv()
-
 # Configuracion API
 URL_SERVER = os.getenv("URL_SERVER")
 API_TOKEN = os.getenv("API_TOKEN")
-#API_URL = f"https://{URL_SERVER}/process_request"
-#RESET_URL = f"https://{URL_SERVER}/reset_record"
+AUDIO_TEMP_FILE = "stream_audio.wav"
 
 # Configuracion Porcupine
 ACCES_KEY = os.getenv("ACCESS_KEY")
@@ -27,26 +23,16 @@ SILENCE_THRESHOLD = 500
 SILENCE_LIMIT_SECONDS = 1.5
 MAX_DURATION_SECONDS = 20
 
-# Configuracion TTS (Piper)
-PIPER_BINARY = "piper"
-VOICE_MODEL = os.path.expanduser("~/piper-voices/es_AR-daniela-high.onnx")
-TTS_OUTPUT_FILE = "respuesta_tts.wav"
+# Configuraciones generales
+START_SOUND_FILE = "config/sound/start_sound.wav"
+FINISH_SOUND_FILE = "config/sound/finish_sound.wav"
 
 # Configuracion SocketIO
-sio = socketio.Client()
+sio = socketio.Client(reconnection=True, reconnection_attempts=5, reconnection_delay=1, request_timeout=20)
 isBusy = False
 
-# Directorios
-# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-# DATA_DIR = os.path.join(SCRIPT_DIR, '..', 'data')
-# AUDIO_FILE_PATH = os.path.join(DATA_DIR, "audio.wav")
-
-
-#os.makedirs(DATA_DIR, exist_ok=True)
-
-
-
 @sio.event
+
 def connect():
     print("Conectado al servidor de la API")
 
@@ -56,49 +42,32 @@ def disconnect():
 
 @sio.event
 def response(data):
+    if 'respuesta_texto' in data:
+        texto = data['respuesta_texto']
+        print(f"Respuesta de texto recibida: {texto}")
+    if 'error'  in data:
+        print(f"Error recibido del servidor: {data['error']}")
+
+@sio.event
+def audio_response(data):
 
     global isBusy
 
-    if 'error' in data:
-        print(f"Error del servidor: {data['error']}")
-
-    text = data.get('respuesta')
-
-    if text:
-        print(f"Respuesta del servidor: {text}")
-        text_to_voice(text)
-
-    isBusy = False
-    print("Esperando Wake Word...\n")
-
-def text_to_voice(text):
-    """
-    Toma un texto, genera audio con Piper y lo reproduce con aplay.
-    """
-    if not text:
-        return
-
-    print(f"Piper hablando: {text}")
-
-    # Se limpia el texto
-    safeText = text.replace('"', '').replace("'", "")
-
-    # Estructura: echo "text" | piper --model ruta_modelo --output_file archivo.wav
-    cmdCommand = f'echo "{safeText}" | {PIPER_BINARY} --model "{VOICE_MODEL}" --output_file "{TTS_OUTPUT_FILE}"'
+    print("Respuesta de audio recibida del servidor.")
 
     try:
-        # Se genera y reproduce el audio
-        subprocess.run(cmdCommand, shell=True, check=True, stderr=subprocess.DEVNULL)
-        subprocess.run(["aplay", TTS_OUTPUT_FILE], stderr=subprocess.DEVNULL)
+        with open(AUDIO_TEMP_FILE, 'wb') as f:
+            f.write(data)
 
-    except subprocess.CalledProcessError as e:
-        print(f"Error en TTS (Piper/Aplay): {e}")
+        subprocess.run(["aplay", AUDIO_TEMP_FILE], stderr=subprocess.DEVNULL)
+
     except Exception as e:
-        print(f"Error inesperado en TTS: {e}")
+        print(f"Error al reproducir audio: {e}")
     finally:
-        # Opcional: Borrar archivo temporal
-        if os.path.exists(TTS_OUTPUT_FILE):
-            os.remove(TTS_OUTPUT_FILE)
+        if os.path.exists(AUDIO_TEMP_FILE):
+            os.remove(AUDIO_TEMP_FILE)
+    isBusy = False
+
 
 def record_and_stream(lengthSeconds = 7):
 
@@ -118,7 +87,7 @@ def record_and_stream(lengthSeconds = 7):
             frame = recorder.read()
             packedFrame = struct.pack("h" * len(frame), *frame)
             sio.emit('audio_chunk', packedFrame)
-
+        subprocess.run(["aplay", FINISH_SOUND_FILE], stderr=subprocess.DEVNULL)
         print("Grabación finalizada.")
         sio.emit('end_of_audio')
 
@@ -130,50 +99,6 @@ def record_and_stream(lengthSeconds = 7):
             recorder.stop()
             recorder.delete()
 
-# def send_audio_API(file_path):
-#     """
-#     Envía un archivo de audio a la API y devuelve la respuesta.
-#     Lanza una excepción si falla.
-#     """
-#     print(f"Enviando archivo de audio a la API: {file_path}")
-#     try:
-#         with open(file_path, 'rb') as audio_file:
-#             files = {'audio': (file_path, audio_file, 'audio/wav')}
-
-#             headers = {'Auth': API_TOKEN}
-#             response = requests.post(API_URL, files=files, headers=headers)
-
-#             response.raise_for_status()
-
-#             print("Archivo enviado, respuesta recibida:")
-#             jsonOutput = response.json()
-#             print(jsonOutput)
-
-
-
-#             # Intenta obtener el text de claves comunes, o usa el JSON entero como string si falla
-#             textToSpeech = jsonOutput.get('response') or \
-#                             jsonOutput.get('respuesta') or \
-#                             jsonOutput.get('text') or \
-#                             jsonOutput.get('mensaje')
-
-#             if textToSpeech:
-#                 text_to_voice(textToSpeech)
-#             else:
-#                 # Si no encuentra clave conocida, imprime aviso (o puedes hacer que lea el json crudo)
-#                 print("No se encontró una clave de text estandar en el JSON para leer.")
-
-#             return jsonOutput
-
-#     except FileNotFoundError:
-#         print(f"Error: Archivo no encontrado en '{file_path}'")
-#         return None
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error en la petición a la API: {e}")
-#         return None
-#     except Exception as e:
-#         print(f"Ocurrió un error inesperado: {e}")
-#         return None
 
 def detect_wake_word():
     """
@@ -182,7 +107,7 @@ def detect_wake_word():
     try:
         porcupine = pvporcupine.create(
             access_key=ACCES_KEY,
-            keywords=[WAKE_WORD]
+            keywords=[WAKE_WORD],
         )
 
         recorder = pvrecorder.PvRecorder(device_index=INDEX_MICROFONO, frame_length=porcupine.frame_length)
@@ -196,6 +121,7 @@ def detect_wake_word():
 
             if output >= 0:
                 print("Wake word detectada!")
+                subprocess.run(["aplay", START_SOUND_FILE], stderr=subprocess.DEVNULL)
                 break
     except KeyboardInterrupt:
         print("Interrumpido por el usuario")
@@ -206,58 +132,6 @@ def detect_wake_word():
         if 'porcupine' in locals():
             porcupine.delete()
 
-# def record_command(audioPath, duracion_segundos = 7):
-#     """
-#     Graba audio desde el micrófono y lo guarda en audioPath.
-#     """
-#     print("Grabando comando de voz...")
-
-#     recorder = None
-#     waveFile = None
-
-#     try:
-#         recorder = pvrecorder.PvRecorder(device_index=INDEX_MICROFONO, frame_length = 512)
-#         waveFile = wave.open(audioPath, 'wb')
-#         waveFile.setnchannels(1)
-#         waveFile.setsampwidth(2)  # 16 bits
-#         waveFile.setframerate(recorder.sample_rate)
-
-#         recorder.start()
-#         print("Grabando...")
-
-#         for _ in range(int(recorder.sample_rate * duracion_segundos / recorder.frame_length)):
-#             frame = recorder.read()
-#             waveFile.writeframes(struct.pack("h" * len(frame), *frame))
-#         print("Grabación finalizada.")
-
-#     except Exception as e:
-#         print(f"Error durante la grabación: {e}")
-#     finally:
-#         if recorder is not None:
-#             recorder.stop()
-#             recorder.delete()
-#         if waveFile is not None:
-#             waveFile.close()
-
-# def reset_conversation():
-#     """
-#     Envía una solicitud al servidor para reiniciar el historial de conversacion.
-#     """
-#     try:
-#         headers = {'Auth': API_TOKEN}
-#         response = requests.post(RESET_URL, headers=headers)
-#         response.raise_for_status()
-#         print("Historial de conversacion reiniciado en el servidor.")
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error al reiniciar el historial en el servidor: {e}")
-
-# if __name__ == "__main__":
-#     reset_conversation()
-#     while(True):
-#         detect_wake_word()
-#         record_command(AUDIO_FILE_PATH)
-#         send_audio_API(AUDIO_FILE_PATH)
-#         print("Reiniciando escucha...\n")
 
 if __name__ == "__main__":
     try:
